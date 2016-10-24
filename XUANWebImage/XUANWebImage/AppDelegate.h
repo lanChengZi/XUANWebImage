8eb035f82568cd2fae4813bcbd861e7201f6acef
徐岸 <ANG@xuan.local>
2016-10-24 11:34:32 +0800

    创建项目

---
+++ XUANWebImage/XUANWebImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XUANWebImage
+//
+//  Created by 徐岸 on 16/10/24.
+//  Copyright © 2016年 xuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XUANWebImage/XUANWebImage/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XUANWebImage
+//
+//  Created by 徐岸 on 16/10/24.
+//  Copyright © 2016年 xuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
